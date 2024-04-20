3703c6e0498464250d7049053c55a66953950e29
Nicholas Nicolis <nicholas.nicolis@gmail.com>
2024-04-21 01:01:31 +0200

    connection front-back end

--- backend/GDSC/models.py
+++ backend/GDSC/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    age = models.IntegerField()
-    school = models.TextField()
-    studyDescription = models.TextField()
-    methodPreferences = models.TextField()
-    studyGoal = models.TextField()
+    age = models.IntegerField(default=18)
+    school = models.TextField(default="")
+    studyDescription = models.TextField(default="")
+    methodPreferences = models.TextField(default="")
+    studyGoal = models.TextField(default="")
 
     def _str_(self):
         return self.age
