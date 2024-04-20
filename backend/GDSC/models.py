cb1ab6ad2a643c0e2fdaa759acd94858184788bf
michelelagreca <michelelagreca.work@gmail.com>
2024-04-21 00:54:58 +0200

    new backend fields

--- backend/GDSC/models.py
+++ backend/GDSC/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    age = models.IntegerField()
+    school = models.TextField()
+    studyDescription = models.TextField()
+    methodPreferences = models.TextField()
+    studyGoal = models.TextField()
 
     def _str_(self):
-        return self.name
+        return self.age
--- backend/GDSC/serializers.py
+++ backend/GDSC/serializers.py
@@ -4,4 +4,4 @@ from .models import Item
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ('id', 'name', 'description')
+        fields = ('age', 'school', 'studyDescription', 'methodPreferences', 'studyGoal')
