5c3d2b3980ee0b48b1d69f05e8f8b30e02600aff
hammersSq <vmartelli56@gmail.com>
2024-04-21 09:02:31 +0200

    new endpoint example for example prompt

--- backend/GDSC/serializers.py
+++ backend/GDSC/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Example
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ('age', 'schoolOrJob', 'studyDescription', 'methodPreference', 'studyGoal')
+
+
+class ExampleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Example
+        fields = ('field')
