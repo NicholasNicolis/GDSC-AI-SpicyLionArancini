d3c94ba2eced711b88c8d1ed4b75396c086d1183
Nicholas Nicolis <nicholas.nicolis@gmail.com>
2024-04-21 11:17:38 +0200

    side

--- frontend/src/component/sidebar.js
+++ frontend/src/component/sidebar.js
@@ -54,7 +54,7 @@ function SideBar() {
                     </a>
 
                     </li>
-
+{/** 
                     <li>
                     <a href="/graphs" class="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group">
                     <svg class="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
@@ -64,7 +64,7 @@ function SideBar() {
                     <span class="ms-3">Graphs</span>
                     </a>
                     </li>
-
+*/}
                     <li>
                     <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
                     <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
