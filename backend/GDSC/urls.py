051fe6cd33fe2cc559f64729a95a5051f3e02090
michelelagreca <michelelagreca.work@gmail.com>
2024-04-21 11:18:15 +0200

    new journey and exercises endpoints

--- backend/GDSC/urls.py
+++ backend/GDSC/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .views import ItemCreateView
 from .views import ItemCreateViewForm
 from .views import Example  
+from .views import Journey
 
 urlpatterns = [
     path('items/', ItemCreateView.as_view(), name='item-create'),
     path('items/form/', ItemCreateViewForm.as_view(), name='item-create-form'),
     path('examples/', Example.as_view(), name='example'),
+    path('journey/', Journey.as_view(), name='journey'),
+    path('exercise/', Exercise.as_view(), name='exercise'),
+
 
 ]
